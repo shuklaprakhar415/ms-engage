e14e6758394ec4071562156167430e59160e96b1
Prakhar <shukla.prakhar415@gmail.com>
2022-05-29 08:52:45 +0530

    sticky navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import icons8 from "../images/icons8.png";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor : "#0B132B" , fontWeight : "bold"}}>
+    <nav className="navbar sticky-top  navbar-expand-lg navbar-dark" style={{backgroundColor : "#0B132B" , fontWeight : "bold"}}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
         <img src={icons8} alt="" width="34" height="34" className="d-inline-block align-text-top mx-2"/>
