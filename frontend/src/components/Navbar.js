1aa8189945db937e0a2953666afd076457a4eaf1
Prakhar <shukla.prakhar415@gmail.com>
2022-05-29 20:49:04 +0530

    cmake

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import icons8 from "../images/icons8.png";
+import {Link} from "react-router-dom"
 
 function Navbar() {
   return (
     <nav className="navbar sticky-top  navbar-expand-lg navbar-dark" style={{backgroundColor : "#0B132B" , fontWeight : "bold"}}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
         <img src={icons8} alt="" width="34" height="34" className="d-inline-block align-text-top mx-2"/>
           Attendance Portal
-        </a>
+        </Link>
       </div>
     </nav>
   )
